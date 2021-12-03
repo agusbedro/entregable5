dac5dce5757f2a07603ef85c5f5cbee4f16d366b
Fermin Maly <ferminmalyvarni@gmail.com>
2021-12-02 22:35:29 -0300

    Agregue chats y haciendo loading

--- js/action.js
+++ js/action.js
@@ -1,2 +1,14 @@
-let email =  document.getElementById('emailRegister').value;
+let botonLogin = document.getElementById('btnLogin');
 
+botonLogin.addEventListener('click', buttonLogin);
+
+function buttonLogin(){
+    let cuerpo = document.getElementById('login');
+    cuerpo.style.display = 'none';
+    let div = document.getElementById('cargaLogin');
+    div.style.display = 'block';
+    setTimeout(function(){window.location.replace('/html/home.html')},4000)
+    //setTimeout(alert('hi'),3000);
+
+    //window.location.replace('/html/home.html')
+};
